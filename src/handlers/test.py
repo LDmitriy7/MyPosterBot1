dd9026cb2f369e54af32c2fd2484650e9d4bbe0b
levch <levchenko.d.a1998@gmail.com>
2022-08-07 01:49:18 +0300

    .

--- src/handlers/test.py
+++ src/handlers/test.py
@@ -1,13 +1,6 @@
-import helpers
-from telebot import on
-
-from assets import models
+from telebot import on, bot
 
 
 @on.command('test', state='*')
 def _():
-    channels = models.Channel.find_all()
-    channel = models.Channel.find(chat_id=-1001500120189)
-
-    print(channels)
-    print(channel)
+    bot.send_message('?', chat_id=7244)
